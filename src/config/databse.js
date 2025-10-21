cfc1c46091564f281938d0ab0c4a8af53680f9aa
Saran Karthik <i.saran.karthik@gmail.com>
2025-10-21 12:07:20 +0530

    Security issues dotenv package installation

--- src/config/databse.js
+++ src/config/databse.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
 // function to connect to our databse dev tinder.
 async function connectDB()
 {
-    await mongoose.connect("mongodb+srv://sarankarthik2510:xz397cxdff@newcluster0.4r1bx.mongodb.net/devTinder");
+    let url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@newcluster0.4r1bx.mongodb.net/devTinder`;    await mongoose.connect(url);
 }
 
 module.exports = connectDB;
