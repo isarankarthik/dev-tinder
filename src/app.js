48fa5cff7f3739de8de0dc9d09a051f061f98f7d
Saran Karthik <i.saran.karthik@gmail.com>
2025-10-21 18:29:38 +0530

    get individual user based on email and all the users

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const User = require("./models/user");
 const app = express();
 // usage of json middleware. JSON middleware converts the data from JSON to javascript object.
 app.use(express.json());
+// create new user to the application.
 app.post("/signup", async(req, res) => {
   try {
   // creation of new instance of the user model. 
@@ -20,6 +21,41 @@ app.post("/signup", async(req, res) => {
     res.status(422).send("Failed to create user.");
   }
 });
+
+// feed api -> this lists all the users in the application.
+app.get("/feed", async(req, res) => {
+  try {
+    let users = await User.find({});
+    if (!users || users.length == 0) {
+      console.error("No users available");
+      return res.status(422).send("Failed to get the users");
+    }
+    return res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    return res.status(422).send("Failed to get the users. Please try again");
+  }
+});
+
+app.get("/user/:email", async(req, res) => {
+  try {
+    // let userEmail = req.query.email;
+    let userEmail = req.params?.email;
+    if (!userEmail) {
+      return res.status(422).send("email is missing");
+    }
+
+    let users = await User.find({email : userEmail});
+    if (!users || users.length == 0) {
+      console.error("Users are not available");
+      return res.status(422).send("Failed to get the users.");
+    }
+    return res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    return res.status(422).send("Failed to get the users. Please try again");
+  }
+})
 
 connectDB().then(() => {
   console.log("DB connection is successful");
